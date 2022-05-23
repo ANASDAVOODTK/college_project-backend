5547c5ba9abd4acc73eafe86802acf5ac538191b
ANASDAVOODTK <anasdavoodtk1@gmail.com>
2022-05-23 09:51:50 +0530

    new update

--- extensions/endpoints/api/index.js
+++ extensions/endpoints/api/index.js
@@ -5,8 +5,35 @@ var request = require('request');
 module.exports = function text(router) {
 	router.get('/', (req, res) => res.send('Hello, World!'));
 	router.get("/postDat",(req, res )=> createStudent())
+  router.get("/postwaste",(req, res) => sendwastedata());
 };
 
+
+const sendwastedata = async (req, res) =>
+{
+  var request = require('request');
+var options = {
+  'method': 'POST',
+  'url': 'https://spector.padippist.tech/items/wastebin',
+  'headers': {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+    "house_name": res.body.house_name,
+    "house_owner": res.body.house_owner,
+    "wastbin_status":  res.body.wastbin_status,
+    "wastbin_persentage": res.body.wastbin_persentage,
+    "phone": res.body.phone,
+    "address":res.body.address
+  })
+
+};
+request(options, function (error, response) {
+  if (error) throw new Error(error);
+  console.log(response.body);
+});
+
+}
 
 
 const createStudent = async (req, res,) => {
@@ -26,7 +53,6 @@ var options = {
     "contents": {
       "en": "Done"
     }
-	console.log(Done);
   })
 
 };
